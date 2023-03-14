0107e147ddccd4f823820234c44f113eefc06508
janelikurm <janelikurm@gmail.com>
2023-03-14 10:49:42 +0200

    game working with input control

--- src/NumberGenerator.java
+++ src/NumberGenerator.java
@@ -17,21 +17,30 @@ public class NumberGenerator {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int secretNumber = random.nextInt(100);
+        int secretNumber = random.nextInt(1, 101);
         System.out.println(secretNumber);
         System.out.println("Hello! I've generated a random number between 1 and 100. Please guess what?");
+        String input = scanner.nextLine();
 
-
-        String input = scanner.nextLine();;
         while (!input.equals("exit")) {
-            if (secretNumber == Integer.parseInt(input)) {
-                System.out.println("Wow! You are a genius!" + " " + secretNumber + " is correct answer!");
-                break;
-            } else if (Integer.parseInt(input) < secretNumber) {
-                System.out.println("The number is bigger than you guessed. Please try again!");
-            } else if (Integer.parseInt(input) > secretNumber) {
-                System.out.println("The number is smaller than you guessed. Please try again!");
-            } input = scanner.next();
+//            input.matches("\\d+");
+            try {
+                int userGuess = Integer.parseInt(input);
+                if (userGuess <= 0 || userGuess > 100) {
+                    System.out.println("The number must be between 1 and 100");
+                } else if (secretNumber == userGuess) {
+                    System.out.println("Wow! You are a genius!" + " " + secretNumber + " is correct answer!");
+                    break;
+                } else if (userGuess < secretNumber) {
+                    System.out.println("The number is bigger than you guessed. Please try again!");
+                } else if (userGuess > secretNumber) {
+                    System.out.println("The number is smaller than you guessed. Please try again!");
+                }
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println("If you would like to continue the game please provide number between 1 to 100. If you would like to finish the game, please write 'exit'.");
+            }
+            input = scanner.next();
         } System.out.println("You have finished the game. Bye!");
     }
 }
+
